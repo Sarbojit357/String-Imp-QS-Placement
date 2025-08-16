411cf7320d94bd315837cf0315fb3e3586949f73
Sarbojit Podder <sarbojitpodder2003@gmail.com>
2025-08-16 20:56:45 +0530

    Sorting without using function

---
+++ sortcode.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class sortcode {
+    public static void main(String[] args) {
+        String str = "programming";
+        char [] ch = str.toCharArray();
+        char temp;
+        for(int i =0; i<ch.length; i++){
+            for(int j = i+1; j<ch.length; j++){
+                if (ch[i]>ch[j]) {
+                    temp = ch[i];
+                    ch[i] = ch[j];
+                    ch[j] = temp;
+                }
+            }
+        }
+        System.out.println(new String(ch));
+    }
+}
